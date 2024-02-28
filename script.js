f82d00b40bb7f6ce15274439c9a3497c1f82a04c
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-28 14:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+// script.js
+function togglePanel(panelNumber) {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+  const panel = document.getElementById(`panel-${panelNumber}`);
+  panel.classList.add('active');
+}
